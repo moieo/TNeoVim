6c90054642245ad99f885d55ac623d86fc57542b
Moieo <moieo@outlook.com>
2023-06-26 13:42:43 +0000

    将插件管理器从 packer.nvim 变更至 lazy.nvim

--- README.md
+++ README.md
@@ -1,27 +1,7 @@
-# 文件结构
-
-```
-├── README.MD # 当前的文档
-├── init.lua # 入口文件，Packer 安装插件在此文件配置
-├── lua
-│   ├── initialization.lua # NeoVim 的基础配置，基本热键，
-│   ├── lsp
-│   │   ├── cmp.lua # cmp 语法提示框的配置
-│   │   ├── handlers.lua # NeoVim 的顶部栏
-│   │   ├── init.lua # LSP 入口配置，添加语法检查可编辑此文件
-│   │   └── keybindings.lua # LSP 的热键
-│   ├── lualine-config.lua # lualine插件，底部状态栏配置文件
-│   ├── navic-config.lua # Navic插件的配置文件
-│   ├── null-ls-config.lua # null-ls插件，语法格式化插件配置文件
-│   ├── nvimtree-config.lua # NeoVim 自带文件树配置文件
-│   └── tabline-config.lua # 标签栏配置文件
-└── plugin
-    └── packer_compiled.lua # Packer 插件管理工具
-```
-
 # 安装过程
 
 ## 准备工作
+
 想要使该配置正常工作，需要有 Nodejs v16 以上 及 Python3.7 以上的支持
 
 安装所需的 Nodejs 包
@@ -37,7 +17,7 @@ npm install --global neovim prettier eslint
 pip3 install pynvim
 ```
 
-可以在完成 NeoVim 安装 及 Packer 所有插件下载完之后在 NeoVim 执行 `:checkhealth` 检查是否缺失需要的软件包，缺失的软件包请自行安装
+NeoVim 所有插件下载完成后在 NeoVim 执行 `:checkhealth` 检查是否缺失需要的软件包，缺失的软件包请自行安装
 
 ## 安装 NeoVim
 
@@ -59,13 +39,6 @@ Arch Linux
 sudo pacman -S neovim
 ```
 
-## 安装 Packer
-
-```bash
-mkdir -p $HOME/.local/share/nvim/site/pack/packer/start
-git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
-```
-
 ## 克隆项目
 
 ```bash
@@ -76,43 +49,26 @@ git clone https://gitee.com/moieo/neo-vim-configure $HOME/.config/nvim
 ## 首次启动 NeoVim
 
 ```bash
-nvim :PackerInstall
+nvim
 ```
-
-如果左侧没有正在执行的下载任务，且有红色报错，请按`q`键关闭报错，并手动在 NeoVim 中执行`:PackerInstall`
 
 首次启动将下载需要的插件以及语法补全所需文件，请保持网络通畅
 
 默认情况下，所有需要的文件都从 Github 克隆/下载
 
 ## 更新/安装 LSP 环境
+
 可以通过 Mason 来手动管理 LSP 环境，也可以打开指定语言的文件，然后执行 `:LspInstall`
 
 # 该配置支持的开发环境
+
 根据自己目前的需求，配置完成后可以支持以下开发
-> Python
 
-> C/C++
-
-> Nodejs
-
-> Vue
-
-> React
-
-> Lua
-
-> Golang
-
-> Shell
-
-> Java
-
-> JSON
-
-> TOML
-
-> MarkDown
+| Python | C/C++ | Nodejs   |
+| ------ | ----- | -------- |
+| Vue    | React | Lua      |
+| Golang | Shell | Java     |
+| JSON   | TOML  | MarkDown |
 
 # 特别感谢
 
--- init.lua
+++ init.lua
@@ -1,152 +1,137 @@
 -- By: Moieo
--- By: Moieo
-vim.cmd([[packadd packer.nvim]])
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://ghproxy.com/github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
-	-- Theme
-	use({
-		"folke/tokyonight.nvim",
-		branch = "main",
-	})
+require("lazy").setup({
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,  -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("initialization")
+    end,
+  },
+  {
+    "kyazdani42/nvim-web-devicons",
+    lazy = true,
+  },
+  -- Lualine
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine-config")
+    end,
+  },
+  {
+    "kdheepak/tabline.nvim",
+    config = function()
+      require("tabline-config")
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    lazy = true,
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("lsp")
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = true,
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp", --neovim 内置 LSP 客户端的 nvim-cmp 源
+      --以下插件可选，可以根据个人喜好删减
+      "onsails/lspkind-nvim", --美化自动完成提示信息
+      "hrsh7th/cmp-buffer", --从buffer中智能提示
+      "hrsh7th/cmp-nvim-lua", --nvim-cmp source for neovim Lua API.
+      -- "octaltree/cmp-look", --用于完成英语单词
+      "hrsh7th/cmp-path",  --自动提示硬盘上的文件
+      -- "hrsh7th/cmp-calc", --输入数学算式（如1+1=）自动计算
+      -- "f3fora/cmp-spell", --nvim-cmp 的拼写源基于 vim 的拼写建议
+      "hrsh7th/cmp-emoji", --输入: 可以显示表情
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-vsnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+  },
+  -- 标签查看器
+  "liuchengxu/vista.vim",
+  -- 语法标签
+  {
+    "SmiteshP/nvim-navic",
+    config = function()
+      require("navic-config")
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("null-ls-config")
+    end,
+  },
+  -- 对齐线
+  "Yggdroot/indentLine",
+  {
+    "nvim-tree/nvim-tree.lua",
+    tag = "nightly",
+    config = function()
+      require("nvimtree-config")
+    end,
+  },
+  -- 语法高亮
+  "leafgarland/typescript-vim",
+  "leshill/vim-json",
+  "vim-python/python-syntax",
+  "posva/vim-vue",
+  "udalov/kotlin-vim",
+  -- 兼容 .vscode/tasks.json
+  "stevearc/overseer.nvim",
+  -- 补全括号
+  "jiangmiao/auto-pairs",
+  -- Terminal 终端
+  {
+    "s1n7ax/nvim-terminal",
+    lazy = true,
+    config = function()
+      require("terminal-config")
+    end,
+  },
 
-	-- Lualine
-	use("nvim-lualine/lualine.nvim")
-	-- tabline
-	use({
-		"kdheepak/tabline.nvim",
-		requires = { { "hoob3rt/lualine.nvim", opt = true }, { "kyazdani42/nvim-web-devicons", opt = true } },
-	})
-	-- If you want to have icons in your statusline choose one of these
-	use("kyazdani42/nvim-web-devicons")
-
-	-- lsp
-	-- use "williamboman/nvim-lsp-installer"
-	-- use 'neovim/nvim-lspconfig'
-	-- use 'williamboman/mason.nvim'
-	-- use 'williamboman/mason-lspconfig.nvim'
-	use({
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	})
-
-	use({
-		"hrsh7th/nvim-cmp",
-		requires = {
-			"hrsh7th/cmp-nvim-lsp", --neovim 内置 LSP 客户端的 nvim-cmp 源
-			--以下插件可选，可以根据个人喜好删减
-			"onsails/lspkind-nvim", --美化自动完成提示信息
-			"hrsh7th/cmp-buffer", --从buffer中智能提示
-			"hrsh7th/cmp-nvim-lua", --nvim-cmp source for neovim Lua API.
-			-- "octaltree/cmp-look", --用于完成英语单词
-			"hrsh7th/cmp-path", --自动提示硬盘上的文件
-			"hrsh7th/cmp-calc", --输入数学算式（如1+1=）自动计算
-			-- "f3fora/cmp-spell", --nvim-cmp 的拼写源基于 vim 的拼写建议
-			"hrsh7th/cmp-emoji", --输入: 可以显示表情
-			"hrsh7th/cmp-cmdline",
-			"hrsh7th/cmp-vsnip",
-			"saadparwaiz1/cmp_luasnip",
-		},
-	})
-
-	use("hrsh7th/vim-vsnip")
-
-	-- 标签查看器
-	use("liuchengxu/vista.vim")
-
-	-- 语法标签
-	-- navic
-	use("SmiteshP/nvim-navic")
-
-	-- 格式化
-	use({
-		"jose-elias-alvarez/null-ls.nvim",
-		config = function()
-			require("null-ls").setup()
-		end,
-		requires = { "nvim-lua/plenary.nvim" },
-	})
-
-	-- 异步语法检查
-	-- use 'dense-analysis/ale'
-
-	-- 对齐线
-	use("Yggdroot/indentLine")
-
-	-- 文件树
-	use({
-		"nvim-tree/nvim-tree.lua",
-		requires = {
-			"nvim-tree/nvim-web-devicons", -- optional, for file icons
-		},
-		tag = "nightly", -- optional, updated every week. (see issue #1193)
-	})
-
-	-- 语法高亮
-	use("leafgarland/typescript-vim")
-	use("leshill/vim-json")
-	use("vim-python/python-syntax")
-	use("posva/vim-vue")
-	use("udalov/kotlin-vim")
-
-	-- 任务插件
-	use({
-		"stevearc/overseer.nvim",
-		config = function()
-			require("overseer").setup()
-		end,
-	})
-
-	-- 补全括号
-	use("jiangmiao/auto-pairs")
-
-	-- Terminal
-	use("s1n7ax/nvim-terminal")
-
-	-- 错误提示
-	use({
-		"folke/trouble.nvim",
-		requires = {
-			"nvim-tree/nvim-web-devicons",
-		},
-	})
-
-  -- 缓冲区浏览器
-  use("jlanzarotta/bufexplorer")
-
-  -- 文件搜索工具
-  use("ibhagwan/fzf-lua")
-end)
--- 初始化配置
-require("initialization")
-
--- navic
-require("navic-config")
-
--- lualine
-require("lualine-config")
-
--- tabline
-require("tabline-config")
-
--- lsp
-require("lsp")
-
--- null-ls
-require("null-ls-config")
-
--- nvim-tree
-require("nvimtree-config")
-
--- null-ls
-require("null-ls-config")
-
--- nvim-tree
-require("nvimtree-config")
-
--- trouble
-require("trouble-config")
-
--- terminal
-require("terminal-config")
+  -- 错误提示
+  {
+    "folke/trouble.nvim",
+    lazy = true,
+    config = function()
+      require("trouble-config")
+    end,
+  },
+  --缓冲区浏览器
+  "jlanzarotta/bufexplorer",
+  -- fzf 文件快速搜索
+  "ibhagwan/fzf-lua",
+}, {
+  git = {
+    log = { "-8" },
+    timeout = 120,
+    url_format = "https://ghproxy.com/github.com/%s.git",
+    filter = true,
+  },
+})
