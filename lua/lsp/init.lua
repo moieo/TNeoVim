9bed7fbeae823375a6b138728f2765a94de8f0e2
moieo <moieo@outlook.com>
2023-06-28 12:53:44 +0000

    增加 lsp 的一些热键

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -26,14 +26,7 @@ local function on_attach(client, bufnr)
 	-- 语法标签
 	navic.attach(client, bufnr)
 	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+  require('lsp.keybindings').maplsp(bufnr)
 end
 
 -- Setup lspconfig.
